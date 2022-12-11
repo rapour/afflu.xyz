c8ef0f531e8d7373f7d71ef28b47ef5138d6aab4
RegulusBit <icdct1995@gmail.com>
2022-12-11 16:17:19 +0330

    blog posts implementation ongoing

---
+++ components/BlogPost.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+
+
+const BlogPost = ({ title, excerpt, slug }) => {
+
+  return (
+    <Link href={`/blog/${slug}`}>
+      <a className="w-full">
+        <div className="mb-8 w-full">
+          <div className="flex flex-col md:flex-row justify-between">
+            <h4 className="text-lg md:text-xl font-medium mb-2 w-full text-gray-900 dark:text-gray-100">
+              {title}
+            </h4>
+            <p className="text-gray-500 text-left md:text-right w-32 mb-4 md:mb-0">
+              {`views`}
+            </p>
+          </div>
+          <p className="text-gray-600 dark:text-gray-400">{excerpt}</p>
+        </div>
+      </a>
+    </Link>
+  );
+};
+
+export default BlogPost;
